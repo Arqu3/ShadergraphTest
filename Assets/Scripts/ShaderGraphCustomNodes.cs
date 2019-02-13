9d71cc4e64e5397505c2a247605bbc85a21471bd
Ludwig <ludwiggustavsson3@gmail.com>
2019-02-13 14:06:36 +0100

    Added project

---
+++ Assets/Scripts/ShaderGraphCustomNodes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ShaderGraph;
+using UnityEditor;
+using System.Reflection;
+
+[Title("Custom", "Refract")]
+public class Refract : CodeFunctionNode
+{
+    public Refract()
+    {
+        name = "Refract";
+    }
+
+    protected override MethodInfo GetFunctionToConvert()
+    {
+        return GetType().GetMethod("refract", BindingFlags.Static | BindingFlags.NonPublic);
+    }
+
+    static string refract(
+        [Slot(0, Binding.None)] DynamicDimensionVector viewDirection,
+        [Slot(1, Binding.None)] DynamicDimensionVector objectNormal,
+        [Slot(2, Binding.None)] Vector1 scalar,
+        [Slot(3, Binding.None)] out DynamicDimensionVector Out
+        )
+    {
+        return @"
+{ 
+    Out = refract(normalize(viewDirection), normalize(objectNormal), 1 / scalar);
+}";
+    }
+}
