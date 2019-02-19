468d5e52e8ee6d02de18207ff1dd2358f0e0c4bc
Ludwig <ludwiggustavsson3@gmail.com>
2019-02-19 10:34:58 +0100

    Added basic showcasing logic to scene

---
+++ Assets/Scripts/Showcasing/ParticleSystemPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public abstract class ParticleSystemPlayer : MonoBehaviour
+{
+    protected ParticleSystem pSystem;
+
+    protected virtual void Awake()
+    {
+        pSystem = GetComponent<ParticleSystem>();
+    }
+
+    public void Play()
+    {
+        pSystem.Play();
+        OnPlay();
+    }
+
+    protected abstract void OnPlay();
+
+    private void OnEnable()
+    {
+        OnSelected();
+    }
+
+    private void OnDisable()
+    {
+        OnDeselected();
+    }
+
+    protected virtual void OnSelected()
+    {
+
+    }
+
+    protected virtual void OnDeselected()
+    {
+        pSystem.Stop();
+    }
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        enabled = false;
+    }
+
+#endif
+}
---
+++ Assets/Scripts/Showcasing/ParticleSystemSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemSelector : MonoBehaviour
+{
+    #region Public variables
+
+    [Header("Particle system players")]
+    [SerializeField]
+    ParticleSystemPlayer[] players;
+
+    #endregion
+
+    Vector3 targetPosition;
+    int selectedIndex = 0;
+
+    private void Awake()
+    {
+        targetPosition = players[selectedIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            players[selectedIndex].enabled = false;
+            int delta = Input.GetKeyDown(KeyCode.A) ? -1 : 1;
+
+            selectedIndex += delta;
+            if (selectedIndex < 0) selectedIndex = players.Length - 1;
+            else if (selectedIndex >= players.Length) selectedIndex = 0;
+
+            players[selectedIndex].enabled = true;
+            targetPosition = players[selectedIndex].transform.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(targetPosition.x, transform.position.y, transform.position.z), 20f * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space)) players[selectedIndex].Play();
+    }
+}
