14b3b4273a4da5bfe206434c46b2498f8f2c74c1
Ludwig <ludwiggustavsson3@gmail.com>
2019-04-30 19:07:50 +0200

    Finished cube juice example

---
+++ Assets/Scripts/Showcasing/Juice/CameraShakeLayer.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+public class GenericUnityEvent<T0, T1, T2> : UnityEvent<T0, T1, T2> { };
+
+public abstract class CameraShakeLayer : MonoBehaviour
+{
+    [Header("Curves")]
+    [SerializeField]
+    protected bool useX = true;
+    [SerializeField]
+    protected AnimationCurve xCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1.0f, 0f));
+
+    [SerializeField]
+    protected bool useY = true;
+    [SerializeField]
+    protected AnimationCurve yCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1.0f, 0f));
+
+    [SerializeField]
+    protected bool useZ = true;
+    [SerializeField]
+    protected AnimationCurve zCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1.0f, 0f));
+
+    public enum ResetMode
+    {
+        None = 0,
+        Lerp = 1
+    }
+
+    [Header("Reset state")]
+    [SerializeField]
+    ResetMode resetMode = ResetMode.None;
+
+    public float Duration
+    {
+        get
+        {
+            return Mathf.Max(useX ? xCurve.keys[xCurve.length - 1].time : 0f, useY ? yCurve.keys[yCurve.length - 1].time : 0, useZ ? zCurve.keys[zCurve.length - 1].time : 0);
+        }
+    }
+
+    protected Vector3 localStartpos;
+    protected Quaternion localStartRot;
+
+    protected readonly UnityEvent onStart = new UnityEvent();
+    protected readonly GenericUnityEvent<float, float, float> onLoop = new GenericUnityEvent<float, float, float>();
+    protected readonly UnityEvent onReset = new UnityEvent();
+    protected readonly UnityEvent onStopLoop = new UnityEvent();
+    protected readonly UnityEvent onStartLoop = new UnityEvent();
+
+    private bool playing = false;
+    private bool playingLoop = false;
+
+    protected virtual void Awake()
+    {
+        localStartpos = transform.localPosition;
+        localStartRot = transform.localRotation;
+    }
+
+    public virtual Coroutine Play(float duration, bool resetWhenDone = true)
+    {
+        return StartCoroutine(_Play(duration, resetWhenDone));
+    }
+
+#if UNITY_EDITOR
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) Play();
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            if (playingLoop) StopLoop();
+            else PlayLoop();
+        }
+    }
+
+#endif
+
+    public virtual Coroutine Play(bool resetWhenDone = true)
+    {
+        return Play(Duration, resetWhenDone);
+    }
+
+    protected IEnumerator _Play(float duration, bool resetWhenDone = true)
+    {
+        if (playing) yield break;
+
+        playing = true;
+
+        float timer = 0.0f;
+        onStart.Invoke();
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            float percentage = timer / duration;
+            onLoop.Invoke(
+                useX ? xCurve.Evaluate(xCurve.keys[xCurve.length - 1].time * percentage) : 0f,
+                useY ? yCurve.Evaluate(yCurve.keys[yCurve.length - 1].time * percentage) : 0f,
+                useZ ? zCurve.Evaluate(zCurve.keys[zCurve.length - 1].time * percentage) : 0f);
+
+            yield return null;
+        }
+
+        if (resetWhenDone) ResetPositionAndRotation();
+        playing = false;
+    }
+
+    public Coroutine PlayLoop()
+    {
+        return StartCoroutine(_PlayLoop());
+    }
+
+    IEnumerator _PlayLoop()
+    {
+        if (playingLoop) yield break;
+
+        onStartLoop.Invoke();
+        playingLoop = true;
+
+        while (true)
+        {
+            float duration = Duration;
+            Play(duration, false);
+            yield return new WaitForSeconds(duration);
+        }
+    }
+
+    public void StopLoop()
+    {
+        StopAllCoroutines();
+        ResetPositionAndRotation();
+        playing = false;
+        playingLoop = false;
+        onStopLoop.Invoke();
+    }
+
+    protected virtual void ResetPositionAndRotation()
+    {
+        switch (resetMode)
+        {
+            case ResetMode.None:
+                transform.localPosition = localStartpos;
+                transform.localRotation = localStartRot;
+                break;
+            case ResetMode.Lerp:
+                StartCoroutine(_ResetSequence());
+                break;
+            default:
+                break;
+        }
+
+        onReset.Invoke();
+    }
+
+    IEnumerator _ResetSequence()
+    {
+        float resetTime = 0.1f;
+        float resetTimer = 0.0f;
+
+        Vector3 resetStartpos = transform.localPosition;
+        Quaternion resetStartrot = transform.localRotation;
+
+        while (resetTimer < resetTime)
+        {
+            resetTimer += Time.deltaTime;
+
+            transform.localPosition = Vector3.Lerp(resetStartpos, localStartpos, resetTimer / resetTime);
+            transform.localRotation = Quaternion.Lerp(resetStartrot, localStartRot, resetTimer / resetTime);
+
+            yield return null;
+        }
+
+        transform.localPosition = localStartpos;
+        transform.localRotation = localStartRot;
+    }
+
+    public bool IsPlaying
+    {
+        get
+        {
+            return playing;
+        }
+    }
+}
--- Assets/Scripts/Showcasing/Juice/CubeSpawner.cs
+++ Assets/Scripts/Showcasing/Juice/CubeSpawner.cs
@@ -1,47 +1,119 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CubeSpawner : MonoBehaviour
 {
     [Header("Prefab")]
     [SerializeField]
     GameObject prefab;
+    [SerializeField]
+    ParticleSystem attachedSystem;
+    [SerializeField]
+    ParticleSystem smokeSystem;
+    [SerializeField]
+    ParticleSystem explosionSystem;
 
     [Header("Spawn variables")]
+    [SerializeField]
+    float initialDelay = 0.5f;
+    [SerializeField]
+    float spawnTime = 0.5f;
     [SerializeField]
     AnimationCurve movementCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1f, 0f));
     [SerializeField]
     AnimationCurve scaleCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1f, 0f));
 
-    GameObject prev;
+    [Header("Hologram preview")]
+    [SerializeField]
+    Material ghostMaterial;
+
+    [Header("Camera")]
+    [SerializeField]
+    List<CameraShakeLayer> shakeLayers = new List<CameraShakeLayer>();
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) Create();
+        if (Input.GetKeyDown(KeyCode.Space)) CreateIterative(3);
     }
 
-    void Create()
+    void CreateIterative(int iterations)
     {
-        if (prev) Destroy(prev);
+        for(int i = 0; i < iterations; ++i)
+        {
+            StartCoroutine(_CreateDelayed(i, prefab.transform.position + Vector3.left * 3f + Vector3.right * i * 3f));
+        }
+    }
 
+    IEnumerator _CreateDelayed(float delay, Vector3 destination, bool skipSequence = false)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Create(destination, skipSequence);
+    }
+
+    void Create(bool skipSequence = false)
+    {
+        Create(prefab.transform.position, skipSequence);
+    }
+
+    void Create(Vector3 destination, bool skipSequence = false)
+    {
         var instance = Instantiate(prefab);
-        StartCoroutine(_SpawnSequence(instance, 0.5f, 0.5f));
-        prev = instance;
+        instance.transform.position = destination;
+        if (skipSequence) return;
+        StartCoroutine(_SpawnSequence(instance, destination, spawnTime, initialDelay));
     }
 
-    IEnumerator _SpawnSequence(GameObject gObj, float spawnTime, float initialDelay)
+    IEnumerator _SpawnSequence(GameObject gObj, Vector3 destination, float spawnTime, float initialDelay)
     {
-        float timer = 0.0f;
-
-        float movementCurveLength = movementCurve[movementCurve.keys.Length - 1].time;
-
-        Vector3 destination = gObj.transform.position;
         Vector3 start = gObj.transform.position - Vector3.up * 3f;
 
         gObj.transform.position = start;
 
-        yield return new WaitForSeconds(initialDelay);
+        var preview = Instantiate(gObj);
+        preview.transform.position = destination;
+        preview.transform.localScale *= 1.01f;
+        var material = new Material(ghostMaterial);
+        preview.GetComponentInChildren<Renderer>().material = material;
+
+        string colorName = "Color_11EBE5FF";
+        string clipName = "Vector1_5AB8A4D";
+
+        Color toColor = material.GetColor(colorName);
+        Color fromColor = new Color(0, 0, 0, 0);
+
+        float toClip = material.GetFloat(clipName);
+        float fromClip = 0.5f;
+
+        float initialTimer = 0.0f;
+        float offsetMulti = 1.5f;
+
+        while (initialTimer < initialDelay)
+        {
+            initialTimer += Time.deltaTime;
+
+            material.SetColor(colorName, Color.Lerp(fromColor, toColor, initialTimer * offsetMulti / initialDelay));
+            material.SetFloat(clipName, Mathf.Lerp(fromClip, toClip, initialTimer * offsetMulti / initialDelay));
+
+            yield return null;
+        }
+
+        float timer = 0.0f;
+        float movementCurveLength = movementCurve[movementCurve.keys.Length - 1].time;
+
+        var attach = Instantiate(attachedSystem);
+        attach.transform.SetParent(gObj.transform.GetChild(0));
+        attach.transform.localPosition = Vector3.zero;
+        attach.Stop();
+        var main = attach.main;
+        main.duration = spawnTime * 1.2f;
+        attach.Play();
+
+        StartCoroutine(_SpawnSystemDelayed(smokeSystem, destination, smokeSystem.transform.rotation, spawnTime * 0.8f));
+        StartCoroutine(_SpawnSystemDelayed(explosionSystem, destination + Vector3.up, explosionSystem.transform.rotation, spawnTime * 0.8f));
+        StartCoroutine(_InvokeFunctionDelayed(() => shakeLayers.ForEach(x => x.Play()), spawnTime * 0.8f));
 
         while (timer < spawnTime)
         {
@@ -52,5 +124,39 @@ public class CubeSpawner : MonoBehaviour
 
             yield return null;
         }
+
+        float exitTime = 0.5f;
+        float exitTimer = 0.0f;
+
+        Vector3 fromScale = gObj.transform.localScale;
+        Vector3 toScale = gObj.transform.localScale * 0.3f;
+
+        while (exitTimer < exitTime)
+        {
+            exitTimer += Time.deltaTime;
+
+            gObj.transform.localScale = Vector3.LerpUnclamped(fromScale, toScale, scaleCurve.Evaluate(exitTimer / exitTime));
+
+            material.SetColor(colorName, Color.Lerp(toColor, fromColor, exitTimer / exitTime));
+            material.SetFloat(clipName, Mathf.Lerp(toClip, fromClip, exitTimer / exitTime));
+
+            yield return null;
+        }
+        if (preview) Destroy(preview);
+        if (material) Destroy(material);
+    }
+
+    IEnumerator _SpawnSystemDelayed(ParticleSystem system, Vector3 position, Quaternion rotation, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Instantiate(system, position, rotation);
+    }
+
+    IEnumerator _InvokeFunctionDelayed(System.Action func, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        func();
     }
 }
